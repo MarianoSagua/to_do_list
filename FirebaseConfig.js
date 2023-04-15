b215bb6282391f1a2bd59858ae079bee48aa1246
MarianoSagua <marianosagua4343@gmail.com>
2023-04-15 14:13:41 -0300

    Commit 1

---
+++ FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwO8tgprX_tsgYPpnH90qx8abKBoNgK7E",
+  authDomain: "spacex4proyect.firebaseapp.com",
+  projectId: "spacex4proyect",
+  storageBucket: "spacex4proyect.appspot.com",
+  messagingSenderId: "128167332961",
+  appId: "1:128167332961:web:4cb4af6a13f4989d44ae20"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
